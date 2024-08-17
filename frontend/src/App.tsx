07e7bf5c1911c94db9a604f18ec0051553817233
peppapig450 <nbgenius1@gmail.com>
2024-08-17 15:20:57 -0700

    use our form and material ui components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Container, Typography, CssBaseline } from "@mui/material";
+import ScraperForm from "./components/ScraperForm";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Typography component="h1" variant="h5">
+        Web Scraper
+      </Typography>
+      <ScraperForm />
+    </Container>
   );
 }
 
