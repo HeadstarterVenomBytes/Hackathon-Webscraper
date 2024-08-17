1d2fadf88b1c5d73ee1142930c445db7c68a38ac
peppapig450 <nbgenius1@gmail.com>
2024-08-17 10:37:26 -0700

    start rewriting the scraper as a class, write constructor, init and close methods

--- src/scraper.ts
+++ src/scraper.ts
@@ -1,27 +1,23 @@
-import puppeteer from "puppeteer";
+import puppeteer, { Browser, Page } from "puppeteer";
+import { ScrapedData } from "./types";
+import { Database } from "./database";
 
-async function scrapeWebsite(url: string): Promise<void> {
-    try {
-        const browser = await puppeteer.launch();
+export class WebScraper {
+  private browser: Browser | null = null;
+  private database: Database;
+  private urlsToScrape: Set<string> = new Set();
+  private scrapedtoUrls: Set<string> = new Set();
 
-        const page = await browser.newPage();
+  constructor(database: Database, initialUrls: string[]) {
+    this.database = database;
+    initialUrls.forEach((url) => this.urlsToScrape.add(url));
+  }
 
-        await page.goto(url);
+  async init(): Promise<void> {
+    this.browser = await puppeteer.launch();
+  }
 
-        await page.waitForSelector('body');
-
-        const paragraphs = await page.evaluate(() => {
-            const elements = document.querySelectorAll('p');
-            return Array.from(elements).map(element => element.textContent);
-        });
-
-        console.log('Scraped paragraphs:', paragraphs);
-
-        await browser.close();
-    } catch (error) {
-        console.error('An error occurred:', error);
-    }
+  async close(): Promise<void> {
+    if (this.browser) await this.browser.close();
+  }
 }
-
-const url = "https://www.olostep.com/"
-scrapeWebsite(url)
